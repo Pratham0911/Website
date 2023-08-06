f0cc6c4a0bc654a6b3c9f8325e9ef8c297fe6339
Pratham Saxena <90039963+Pratham0911@users.noreply.github.com>
2023-08-07 02:19:46 +0530

    bull.sh

--- bull.sh
+++ bull.sh
@@ -1,8 +1,5 @@
 ls
 docker images
-docker rmi -f maria
-docker ps
-docker kill s2
-docker build -t mariawe .
+docker pull pratham0911/mariawebsite:latest
 docker images
-docker run -itd -p 80:80 --name s3 maria
+docker run -itd -p 80:80 --name s3 pratham0911/mariawebsite
