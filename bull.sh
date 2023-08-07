b6fa91416c1f769f2d22915f29455a3b054429d9
Pratham Saxena <90039963+Pratham0911@users.noreply.github.com>
2023-08-07 14:32:17 +0530

    Update bull.sh

--- bull.sh
+++ bull.sh
@@ -2,4 +2,5 @@ ls
 docker images
 docker pull pratham0911/mariawebsite:latest
 docker images
-docker run -itd -p 80:80 --name s4 pratham0911/mariawebsite
+docker kill s4
+docker run -itd -p 80:80 --name s5 pratham0911/mariawebsite
