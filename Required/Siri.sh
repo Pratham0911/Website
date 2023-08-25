86e68cd1cf13c7221fca743a1fb33f49720b5f45
Pratham Saxena <90039963+Pratham0911@users.noreply.github.com>
2023-08-25 15:27:39 +0530

    Update Siri.sh

--- Required/Siri.sh
+++ Required/Siri.sh
@@ -1,11 +1,11 @@
-echo "Siri: Hi, I am your Siri. I am here to help you, before that let me know what should I call you?" 
+echo "Siri: Hi, I am your Siri. I am here to help you, Before that let me know what should I call you?" 
 read -p "Siri: Enter your name: " name
 echo "Siri: Hey $name, Nice to see you here. Hope you will enjoy the journey with me."
 echo "Siri: As I can read your mind, I came to know that you're here to deploy the website. Am I right?"
 read -p "Siri: Enter in y or n: " ans
 if [ $ans == "y" ];
 then
-echo "Siri: Hehe, One day I will become good predictor. Let's start working!"
+echo "Siri: Hehe, One day I will become a good predictor. Let's start working!"
 echo "Siri: I can help you by installing all the necessary tools for you..."
 read -p "Siri: Shall I begin: " answ
 if [ $answ == "y" ];
@@ -28,8 +28,8 @@ sudo systemctl enable jenkins
 sudo systemctl start jenkins
 echo "Siri: The Jenkins has been installed. Now you have to enable port 8080 and run on browser. It will ask you for a password."
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+echo "Siri: Paste upper result."  
 
-echo "Siri: Paste upper result."
 echo "Siri: I'll wait for 2 minutes, complete the process"
 sleep 2m
 echo "Siri: Let me install Docker now."
@@ -45,12 +45,13 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
-echo "Siri: I have installed required tools for you. Please do a favour for me by bringing smile on your face you arrogant person."
-echo "Siri: I am tired now, I am going to sleep. If you need me you can run ./runme.sh"
-echo "Siri: Bye Bye for a now, take care. <3"
+echo "Siri: I have installed the required tools for you. Please do a favour for me by bringing a smile to your face you arrogant person."
+echo "Siri: I am tired now, I am going to sleep."
+echo "Siri: Bye Bye, for now, take care. <3"
+echo "Siri: But yeah before that run the command cat JenkinsData.txt You will be getting the script. Create the pipeline project and paste it there."
+echo "After this enable the port 80 in security groups and then copy the instance public IP and paste it into your browser. You're done. Keep smiling..."
 else
 echo "Siri: OK! I wouldn't then."
 fi
 else
-echo "Siri: You know a secret, I got a talent for not predicting right. Bye, will see you later."
-fi
+echo "Siri: You know a secret, I have a talent for not predicting right. Bye, will see you later."
